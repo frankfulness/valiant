4234c1c11d3fada17536f15fb60d1c9c9d977df1
Frankfulness <Fullstackfrank@gmail.com>
2024-12-21 21:31:03 -0800

    Add flask endpoints for user, Add flask.dockerfile

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from typing import MutableMapping
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -73,4 +74,38 @@ def get_user():
     except Exception as e:
         return make_response(
             jsonify({"message": "error getting user", "error": str(e)}), 500
+        )
+
+
+# update a user by id
+@app.route("api/flask/users/<id>", methods=["PUT"])
+def update_user():
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            data = request.get_json()
+            user.name = data["name"]
+            user.email = data["email"]
+            db.session.commit()
+            return make_response(jsonify({"message": "user updated"}), 200)
+        return make_response(jsonify({"message": "user not found"}), 404)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": "error updating user", "error": str(e)}), 500
+        )
+
+
+# delete a user by id
+@app.route("api/flask/users/<id>", methods=["DELETE"])
+def delete_user():
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return make_response(jsonify({"message": "user deleted"}), 200)
+        return make_response(jsonify({"message": "user not found"}), 404)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": "error deleting user", "error": str(e)}), 500
         )
